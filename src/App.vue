6ad099a0364a07a39ce22f0d1f9b1bc7457f9b79
Damián Mac Dougall <dmacdougall@teceng-gaming.com.ar>
2023-04-03 23:44:55 -0300

    initial comit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    {{greeting}}
+  </div>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  data(){
+    return{
+      greeting : 'Netlify is great!'
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-size: 30px;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
